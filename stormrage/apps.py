af938b1f6794f36c9009abf0011c889ca604ec7b
timmashkov <bodomizer#yandex.ru>
2023-06-29 17:02:41 +0300

    made third migration, added new features to the admin panel, added images and improved the view of models

--- stormrage/apps.py
+++ stormrage/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class StormrageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stormrage'
+    verbose_name = 'Stormrage application'
+
--- stormrage/models.py
+++ stormrage/models.py
@@ -4,13 +4,18 @@ from django.urls import reverse
 
 class Alpha(models.Model):
     objects = None
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=False)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, verbose_name='Name')
+    content = models.TextField(blank=False, verbose_name='Biography')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Creation time')
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     cat = models.ForeignKey('Fraction', on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        verbose_name = 'Warcraft heroes'
+        verbose_name_plural = 'Warcraft heroes'
+        ordering = ['title']
 
     def __str__(self):
         return self.title
@@ -21,7 +26,12 @@ class Alpha(models.Model):
 
 class Fraction(models.Model):
     objects = None
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Fraction')
+
+    class Meta:
+        verbose_name = 'Fractions'
+        verbose_name_plural = 'Fractions'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
